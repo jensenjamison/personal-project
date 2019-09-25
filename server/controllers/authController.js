f970b3eff75f29b9450845a157126dc953c3e11d
jensenjamison <jensenjamison@yahoo.com>
2019-09-25 18:22:10 -0500

    server setup

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,9 +2,9 @@
 
 module.exports = {
     getSession: (req, res) => {
-    
-
-
+        if(req.session.user){
+            res.status(200).json(req.session.user)
+        }
     },
     register: (req, res) => {
 
--- server/controllers/surveysController.js
+++ server/controllers/surveysController.js
@@ -1,8 +1,10 @@
 
 
 module.exports = {
-    getAll: (req, res) => {
-
+    getAll: async (req, res) => {
+        const db = req.app.get('db');  // bring in db
+// fire our getAll sql statement
+// return (res)  the results from firing our sql statement
 
 using a sql statement, get and  return all surveys
 

81f601634476b46b6bcb43096583a8712ed10b72
jensenjamison <jensenjamison@yahoo.com>
2019-10-07 17:02:31 -0500

    login styling

--- src/components/LoginUser.js
+++ src/components/LoginUser.js
@@ -1,49 +1,53 @@
 import React from "react"
-import {connect} from "react-redux"
-import {loginUser} from "../redux/reducers/userReducer"
+import { connect } from "react-redux"
+import { loginUser } from "../redux/reducers/userReducer"
+import "./LoginUser.css"
+
 
 class LoginUser extends React.Component {
-    constructor(){
+    constructor() {
         super()
-        this.state ={
+        this.state = {
             email: "jamisonjensen1999@gmail.com",
             password: "123"
         }
-        this.handleChange=this.handleChange.bind(this)
-        this.handleSubmit=this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
- 
-    handleChange(e){
+
+    handleChange(e) {
         this.setState({
             [e.target.name]: e.target.value
         })
     }
     handleSubmit(e) {
         e.preventDefault();
-        this.props.loginUser(this.state).then (() => this.props.history.push("/"))
+        this.props.loginUser(this.state).then(() => this.props.history.push("/"))
     }
 
-    render(){
-        return(
-            <div>
-                <h2>Login</h2>
-                <form onSubmit={this.handleSubmit}>
-                <label>Email</label>
-                    <input 
-                        type="text"
-                        name="email"
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                    />
-                    <label>Password</label>
-                    <input 
-                        type="password"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                    />
-                    <button type="submit">Login</button>
-                </form>
+    render() {
+        return (
+            <div className="login-page">
+                <div className="login-box">
+                    <h2 className="login-text">Login</h2>
+                    <form onSubmit={this.handleSubmit} className="login-input">
+                        <label>Email</label>
+                        <input
+                            type="text"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
             </div>
         )
     }
